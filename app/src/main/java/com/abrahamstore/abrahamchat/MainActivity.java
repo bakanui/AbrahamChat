1388de1f498284815a9694afdf9de8d33536b511
Bhaskara Ida Bagus <gusbhas@gmail.com>
2017-11-13 17:01:28 +0700

    Trying register with token

--- app/src/main/java/com/abrahamstore/abrahamchat/MainActivity.java
+++ app/src/main/java/com/abrahamstore/abrahamchat/MainActivity.java
@@ -1,15 +1,38 @@
 package com.abrahamstore.abrahamchat;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
     //defining views
-    private Button buttonDisplayToken;
-    private TextView textViewToken;
+    private Button buttonRegister;
+    private EditText editTextEmail;
+    private ProgressDialog progressDialog;
+
+    //URL to RegisterDevice.php
+    private static final String URL_REGISTER_DEVICE = "http://192.168.1.4/abrahamchat/RegisterDevice.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,27 +40,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         //getting views from xml
-        textViewToken = (TextView) findViewById(R.id.textViewToken);
-        buttonDisplayToken = (Button) findViewById(R.id.buttonDisplayToken);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
 
         //adding listener to view
-        buttonDisplayToken.setOnClickListener(this);
+        buttonRegister.setOnClickListener(this);
+    }
+
+    //storing token to mysql server
+    private void sendTokenToServer() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Registering Device...");
+        progressDialog.show();
+
+        final String token = SharedPrefManager.getInstance(this).getDeviceToken();
+        final String email = editTextEmail.getText().toString();
+
+        if (token == null) {
+            progressDialog.dismiss();
+            Toast.makeText(this, "Token not generated", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGISTER_DEVICE,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+                        try {
+                            JSONObject obj = new JSONObject(response);
+                            Toast.makeText(MainActivity.this, obj.getString("message"), Toast.LENGTH_LONG).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.dismiss();
+                        Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", email);
+                params.put("token", token);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
     @Override
     public void onClick(View view) {
-        if (view == buttonDisplayToken) {
-            //getting token from shared preferences
-            String token = SharedPrefManager.getInstance(this).getDeviceToken();
-
-            //if token is not null
-            if (token != null) {
-                //displaying the token
-                textViewToken.setText(token);
-            } else {
-                //if token is null that means something wrong
-                textViewToken.setText("Token not generated");
-            }
+        if (view == buttonRegister) {
+            sendTokenToServer();
         }
     }
 }
