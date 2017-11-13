b56f4fabec3cc70cedb5370bdc769f2bad8187e5
Bhaskara Ida Bagus <gusbhas@gmail.com>
2017-11-13 15:21:39 +0700

    Trying to retrieve token

--- app/src/main/java/com/abrahamstore/abrahamchat/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/abrahamstore/abrahamchat/MyFirebaseInstanceIDService.java
@@ -1,8 +1,32 @@
 package com.abrahamstore.abrahamchat;
 
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
 /**
  * Created by bakan on 11/13/2017.
  */
 
-public class MyFirebaseInstanceIDService {
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        //Getting registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //Displaying token on logcat
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        //calling the method store token and passing token
+        storeToken(refreshedToken);
+    }
+
+    private void storeToken(String token) {
+        //we will save the token in sharedpreferences later
+        SharedPrefManager.getInstance(getApplicationContext()).saveDeviceToken(token);
+    }
 }
